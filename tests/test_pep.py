b732f6dfa0f84e1bbb55a5ab72d8a613dd859705
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-12-15 22:46:50 +0100

    add tests from the pep

--- tests/test_pep.py
+++ tests/test_pep.py
@@ -5,6 +5,28 @@ ok = [
         '0.0.0rc0.post0.dev0',
     ]
 
+# from pep440:
+ok.extend([
+    "1.0.dev456",
+    "1.0a1",
+    "1.0a2.dev456",
+    "1.0a12.dev456",
+    "1.0a12",
+    "1.0b1.dev456",
+    "1.0b2",
+    "1.0b2.post345.dev456",
+    "1.0b2.post345",
+    "1.0rc1.dev456",
+    "1.0rc1",
+    "1.0",
+#     "1.0+abc.5", # Not sure if version segment is canonical
+#     "1.0+abc.7",
+#     "1.0+5",
+    "1.0.post456.dev34",
+    "1.0.post456",
+    "1.1.dev1",
+])
+
 not_ok = [
         '1!2.3.4.rc5.post6.dev7', # rc is not dot separated
         '0!0.0.0rc0.post0.dev0', # 0 epoch is not canonical
